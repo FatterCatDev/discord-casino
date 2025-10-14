cf9e6a50868c7c9f64c261948a3a1fbeecf648d2
FatterCatDev <kevin_y_jones@hotmail.com>
2025-10-14 17:52:38 +0000

    Auto update: 2025-10-14T17:52:38+00:00

--- src/index.mjs
+++ src/index.mjs
@@ -512,7 +512,7 @@ client.on(Events.InteractionCreate, async interaction => {
 
       }
     // ========== BUTTONS ==========
-    else if (interaction.isButton() && interaction.customId.startsWith('jobshift|')) {
+    else if ((interaction.isButton() || interaction.isStringSelectMenu()) && interaction.customId.startsWith('jobshift|')) {
       const ctx = buildCommandContext(interaction, ctxExtras);
       const mod = await import('./interactions/jobs/shiftButtons.mjs');
       return mod.default(interaction, ctx);
